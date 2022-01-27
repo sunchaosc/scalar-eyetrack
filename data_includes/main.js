0e541f5542e26896b4a4effaf7476c2709d0e16f
Chao Sun <c.sun@leibniz-zas.de>
2022-01-27 11:20:27 +0100

    updated consent

--- data_includes/main.js
+++ data_includes/main.js
@@ -508,9 +508,8 @@ SendResults()
 newTrial("FinalPage",
     exitFullscreen()
     ,
-    newText("Final","<p>You can download a copy of the information sheet by clicking on this link:
-    <a href='https://pcibex.research-zas.de/ibexfiles/scalar/online_adult_behavior.pdf' target='_blank'> Information sheet</a> <br> This is the end of the experiment. <strong> Please verify your participation on Prolific by clicking on this link:  </strong> <br> Thank you for your participation!</p>")
-    ,
+    newText("Final","<p>You can download a copy of the information sheet by clicking on this link:<a href='https://pcibex.research-zas.de/ibexfiles/scalar/online_adult_behavior.pdf' target='_blank' > Information sheet</a></p> <p><br> This is the end of the experiment. <strong> Please verify your participation on Prolific by clicking on this link:  </strong> <br> Thank you for your participation!</p>")
+   ,
     // newText("<p><a href='https://app.prolific.co/submissions/complete?cc=CODE'+ GetURLParameter("id")+"' target='_blank'>Click here to confirm your participation on Prolific.</a></p> <p>This is a necessary step in order for you to receive participation credit!</p>"),
 
     newCanvas("myCanvas", "60vw" , "60vh")
